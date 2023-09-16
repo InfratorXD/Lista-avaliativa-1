428a684e3c6607f24f32cc78d02e8157477f6fb5
InfratorXD <guiguicazeee@gmail.com>
2023-09-15 23:49:24 -0300

    first commit

---
+++ Questão 1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+int x;
+
+for(int i = 1800;i < 2022;i++){
+    
+    scanf("%d",&x);
+    if(x == 1916 || x == 1940 || x == 1944 || x == 1946){
+        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n",x);
+    }
+    else if(x >= 1930 && x <= 2022 && (x - 1930)%4 == 0){
+        printf("A Copa do Mundo de Futebol ocorreu no ano de %d.\n",x);
+    }
+    else if(x >= 1896 && x <= 2022 && (x - 1896)%4 == 0){
+        printf("Os Jogos Olimpicos de Verao ocorreram no ano de %d.\n",x);
+    }
+    else{
+        printf("Nao houve Jogos Olimpicos de Verao ou Copa do Mundo no ano de %d.\n",x);
+    }
+}
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# Lista-avaliativa-1" 
